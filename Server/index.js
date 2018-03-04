fe5f1d645e350c9e2fae8bb868c7aba1dd36fe81
saiteja1303 <saitejachinny242@gmail.com>
2018-03-03 23:18:44 -0600

    hot-loaders

--- Server/index.js
+++ Server/index.js
@@ -1,9 +1,23 @@
 import express from "express";
 import path from 'path';
-var app = express();
+
+import webpack from 'webpack';
+import webpackMiddleware from 'webpack-dev-middleware';
+import webpackConfig from '../webpack.config.dev';
+import webpackHotMiddleware from 'webpack-Hot-middleware';
+
+let app = express();
+
+const compiler = webpack(webpackConfig);
+app.use(webpackMiddleware(compiler, {
+    hot: true,
+    publicPath: webpackConfig.output.publicPath,
+    noInfo: true
+}));
+app.use(webpackHotMiddleware(compiler));
 
 app.get('/*', (req, res) => {
-    res.sendFile(path.join(__dirname, './index.html'));
+    res.sendFile(path.join(__dirname, 'index.html'));
 });
 
 app.listen(1303, () => console.log("Running on localhost: 1303"));
